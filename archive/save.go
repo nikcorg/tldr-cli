1228f0845b38650248483aec001f3bc621ccbf67
Niklas Lindgren <niklas@musicglue.com>
2018-09-24 19:42:26 +0100

    feat: check archive dir exists

--- archive/save.go
+++ archive/save.go
@@ -3,6 +3,8 @@ package archive
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -15,7 +17,21 @@ func Save(filename string, a Archive) {
 		panic(err)
 	}
 
+	ensureArchiveDir(path.Dir(filename))
+
 	if err := ioutil.WriteFile(filename, yml, 0644); err != nil {
-		panic(fmt.Errorf("Error writing %s: %v", filename, err))
+		panic(fmt.Errorf("error writing %s: %v", filename, err))
+	}
+}
+
+func ensureArchiveDir(path string) {
+	if info, err := os.Stat(path); err != nil {
+		if err := os.MkdirAll(path, 0755); err != nil {
+			panic(err)
+		}
+	} else if !info.IsDir() {
+		panic(fmt.Errorf("not a directory: %s", path))
+	} else if info.Mode() != 0755 {
+		panic(fmt.Errorf("wrong mode set on archive dir %s", info.Mode()))
 	}
 }
