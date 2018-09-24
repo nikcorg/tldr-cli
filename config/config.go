4c58d8ffaa9ec094dc90e5ba26b1f3a015a2b784
Niklas Lindgren <niklas@musicglue.com>
2018-09-24 16:16:33 +0100

    feat: add TitleFormat to config

--- config/config.go
+++ config/config.go
@@ -2,10 +2,8 @@ package config
 
 // Config is a configuration
 type Config struct {
-	// Home is the path to TLDR_HOME
-	Home string
-	// Home is the path to TLDR_HOME/archive
-	Archive string
-	// Format is a Dateformat string used to construct new archive filenames
-	Format string
+	Home        string // Home is the path to TLDR_HOME
+	Archive     string // Home is the path to TLDR_HOME/archive
+	Format      string // Format is a Dateformat string used to construct new archive filenames
+	TitleFormat string // TitleFormat is a DateFormat string for daily archive titles
 }
