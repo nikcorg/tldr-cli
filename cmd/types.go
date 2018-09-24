09f06877aaf784d17b9e382f3c70aa91a5ac73e3
Niklas Lindgren <niklas@musicglue.com>
2018-09-24 19:05:52 +0100

    feat: return cli.Command instead of RunnableCommand

--- cmd/types.go
+++ cmd/types.go
@@ -1,13 +1,17 @@
 package cmd
 
-import "github.com/nikcorg/tldr-cli/config"
+import (
+	"github.com/nikcorg/tldr-cli/config"
+
+	"github.com/urfave/cli"
+)
 
 // CommandMap is a map of commands
 type CommandMap map[string]Command
 
 // Command is a container for configuration
 type Command interface {
-	Configure(*config.Config) RunnableCommand
+	Configure(*config.Config) cli.Command
 }
 
 // RunnableCommand is a command you can run
