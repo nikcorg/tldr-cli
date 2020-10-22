9b6259874516936cd69d6f5927084494f7d3afef
Niklas Lindgren <niklas.lindgren@iki.fi>
2020-10-22 23:31:20 +0100

    feat: help for version

--- cli/tldr/version.go
+++ cli/tldr/version.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/MakeNowJust/heredoc"
+)
 
 type versionCmd struct{}
 
@@ -23,4 +28,11 @@ func (c *versionCmd) ParseArgs(subcommand string, args ...string) error {
 	return nil
 }
 
-func (c *versionCmd) Help(subcommand string, args ...string) {}
+func (c *versionCmd) Help(subcommand string, args ...string) {
+
+	fmt.Printf(strings.Replace(heredoc.Doc(`
+		Show the version number and various build time details
+
+		__BINARY_NAME__ version
+	`), "__BINARY_NAME__", binaryName, -1))
+}
