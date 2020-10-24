bbec600be5e9a0569e1e34d5c97427487bde0ca8
Niklas Lindgren <niklas@musicglue.com>
2020-10-24 16:38:00 +0100

    feat: usage for sync

--- cli/tldr/sync.go
+++ cli/tldr/sync.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/MakeNowJust/heredoc"
 	"github.com/nikcorg/tldr-cli/config/rotation"
 	"github.com/nikcorg/tldr-cli/storage"
 	"github.com/nikcorg/tldr-cli/sync"
@@ -20,7 +22,19 @@ func (s *syncCmd) ParseArgs(subcommand string, args ...string) error {
 	return nil
 }
 
-func (s *syncCmd) Help(subcommand string, args ...string) {}
+func (s *syncCmd) Help(subcommand string, args ...string) {
+	fmt.Printf(strings.Replace(heredoc.Doc(`
+		Sync the tldr log with a remote peer
+
+		__BINARY_NAME__ sync
+
+		Sync runs an external command and provides to it as arguments:
+		- the directory containing the tldr log files
+		- the log file to sync
+
+		Currently only syncing a single log file is implemented
+	`), "__BINARY_NAME__", binaryName, -1))
+}
 
 func (s *syncCmd) Execute(subcommand string, args ...string) error {
 	if runtimeConfig.Sync.Exec == "" && runtimeConfig.Sync.Remote != "" {
