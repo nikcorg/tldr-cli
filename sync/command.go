665562ebbb601dcd3b6cb3dcc550d0b248095b03
Niklas Lindgren <niklas@musicglue.com>
2020-10-24 16:34:58 +0100

    feat: simple command sync

--- sync/command.go
+++ sync/command.go
@@ -1,21 +1,37 @@
 package sync
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
 	"syscall"
 
 	"github.com/nikcorg/tldr-cli/storage"
+	log "github.com/sirupsen/logrus"
+)
+
+// Known error outcomes
+var (
+	errSyncCommandNotConfigured = errors.New("no sync command is configured")
 )
 
 // WithCommand executes an external program to sync the local with a remote
 func (s *Sync) WithCommand(sources []*storage.Source) error {
-	var args []string = []string{s.config.Storage.Path}
+	if s.config.Sync.Exec == "" {
+		return errSyncCommandNotConfigured
+	}
+
+	var args = []string{path.Base(s.config.Sync.Exec), s.config.Storage.Path}
 
 	for _, source := range sources {
 		args = append(args, source.SourceFile)
 	}
 
-	if err := syscall.Exec(s.config.Sync.Exec, args, []string{}); err != nil {
-		return err
+	log.Debugf("running %s with args %v", s.config.Sync.Exec, args)
+
+	if err := syscall.Exec(s.config.Sync.Exec, args, os.Environ()); err != nil {
+		return fmt.Errorf("running sync command failed: %w", err)
 	}
 
 	return nil
