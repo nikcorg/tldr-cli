1c9bad16c61c57f1fc5adad3f3a540779e74d78c
Niklas Lindgren <niklas.lindgren@iki.fi>
2020-10-27 12:45:30 +0000

    feat: add RemoveEntry method on Storage

--- storage/source.go
+++ storage/source.go
@@ -1,7 +1,13 @@
 package storage
 
 import (
+	"errors"
+	"reflect"
 	"time"
+)
+
+var (
+	errEntryNotFound = errors.New("entry not found")
 )
 
 // Source represents a set of records on disk
@@ -24,4 +30,17 @@ func (s *Source) WasSynced() bool {
 // FirstRecord returns the first record
 func (s *Source) FirstRecord() *Record {
 	return s.Records[0]
+}
+
+// RemoveEntry finds and removes an entry from a source
+func (s *Source) RemoveEntry(e *Entry) error {
+	for _, r := range s.Records {
+		for i, re := range r.Entries {
+			if reflect.DeepEqual(re, e) {
+				r.Entries = append(r.Entries[:i], r.Entries[(i+1):]...)
+				return nil
+			}
+		}
+	}
+	return errEntryNotFound
 }
