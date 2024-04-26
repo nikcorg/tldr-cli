b19365b8d6ed2bbf5b017f2ff63e1e013d1e3d3b
Niklas Lindgren <niklas.lindgren@iki.fi>
2024-04-26 17:20:07 +0300

    feat(fetch): set user agent header

--- fetch/any.go
+++ fetch/any.go
@@ -1,8 +1,10 @@
 package fetch
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"golang.org/x/net/html"
 
@@ -11,12 +13,24 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func any(url string) (*Details, error) {
-	var err error
-	var res *http.Response
+const userAgent = "TL;DR cli/1.0"
 
-	if res, err = http.Get(url); err != nil {
-		log.Debugf("failed fetching %s: %v", err)
+func any(url string) (*Details, error) {
+	var (
+		err error
+		res *http.Response
+		req *http.Request
+	)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	req, _ = http.NewRequestWithContext(ctx, "GET", url, nil)
+
+	req.Header.Add("User-Agent", userAgent)
+
+	if res, err = http.DefaultClient.Do(req); err != nil {
+		log.Debugf("failed fetching %s: %v", url, err)
 		return nil, err
 	} else if res.StatusCode != 200 {
 		return nil, fmt.Errorf("failed fetching %s: %v", url, res.StatusCode)
